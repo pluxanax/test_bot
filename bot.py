e35be9ee5d0af6e3315a169e58c8d53d2d2af775
pluxanax <yajdytebya@gmail.com>
2022-10-22 14:27:17 +0700

    get btc from yobit.net

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
-import json
 import os
+from time import sleep
 
 import requests
 
+from yobit import get_btc
+
 TOKEN = os.getenv('TOKEN')
 URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+global last_update_id
+last_update_id = 0
 
 
 def get_updates():
@@ -13,10 +17,42 @@ def get_updates():
     return r.json()
 
 
+def get_message():
+    data = get_updates()
+
+    last_object = data['result'][-1]
+    current_update_id = last_object['update_id']
+
+    global last_update_id
+    if last_update_id != current_update_id:
+        last_update_id = current_update_id
+        chat_id = last_object['message']['chat']['id']
+        message_text = last_object['message']['text']
+
+        message = {'chat_id': chat_id, 'text': message_text}
+
+        return message
+    return None
+
+
+def send_message(chat_id, text='Wait a second, please...'):
+    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+    requests.get(url)
+
+
 def main():
-    d = get_updates()
-    with open('updates.json', 'w') as f:
-        json.dump(d, f, indent=2, ensure_ascii=False)
+    while True:
+        answer = get_message()
+
+        if answer != None:
+            chat_id = answer['chat_id']
+            text = answer['text']
+
+            if text == '/btc':
+                send_message(chat_id, get_btc())
+        else:
+            continue
+        sleep(2)
 
 
 if __name__ == '__main__':
